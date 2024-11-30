7d640432d159e8f2a76da16257ab0c40f767e352
Vortex <siftullah01@gmail.com>
2024-11-30 17:38:39 +0500

    another minor change to fix the code

--- src/LoginApp.java
+++ src/LoginApp.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Objects;
 
 public class LoginApp extends JFrame {
     public JTextField emailField;
